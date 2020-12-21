687be5a4baddfe6a499fceff866b86c91e0b51c8
Mikolaj Golowski <mikolaj.golowski@student.uj.edu.pl>
2020-12-21 12:35:04 +0100

    Revert "do usuniecia ten commit"
    This reverts commit 2a895d93

--- os/final/code/sterowanie/Sterowanie.h
+++
@@ -1,8 +0,0 @@
-#ifndef NIEKONCZACE_SIE_SKAKANIE_STEROWANIE_H
-#define NIEKONCZACE_SIE_SKAKANIE_STEROWANIE_H
-#include "../kernel/Engine.h"
-#include "../savegame/save.h"
-
-short InputZKlawiatury(gra_t* gra);
-
-#endif //NIEKONCZACE_SIE_SKAKANIE_STEROWANIE_H
